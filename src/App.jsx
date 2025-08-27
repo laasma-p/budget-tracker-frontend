f0e32af31c227f9f0f274f0740b8a8be86e584f0
Lasma Poksane <laasmapoksane@gmail.com>
2025-08-27 20:43:39 +0200

    Test tailwindcss if the import worked

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex text-2xl justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
